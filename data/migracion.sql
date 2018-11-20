3d1f0849056ad7cdf32055aea8dcd0cf32b329f8
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-11-20 18:30:12 -0300

    varios fixes... faltan items y compras del todo bien

--- data/migracion.sql
+++ data/migracion.sql
@@ -21,20 +21,18 @@ insert into empresas (
   numero,
   piso,
   departamento,
-  localidad,
   codigo_postal,
-  ciudad,
   cuit
 )
 select distinct
-  Espec_Empresa_Razon_Social as razon_social,
-  Espec_Empresa_Mail as mail,
-  Espec_Empresa_Dom_Calle as calle,
-  Espec_Empresa_Nro_Calle as numero,
-  Espec_Empresa_Piso as piso,
-  Espec_Empresa_Depto as departamento,
-  Espec_Empresa_Cod_Postal as codigo_postal,
-  Espec_Empresa_Cuit as cuit
+  Espec_Empresa_Razon_Social,
+  Espec_Empresa_Mail,
+  Espec_Empresa_Dom_Calle,
+  Espec_Empresa_Nro_Calle,
+  Espec_Empresa_Piso,
+  Espec_Empresa_Depto,
+  Espec_Empresa_Cod_Postal,
+  Espec_Empresa_Cuit
 from gd_esquema.Maestra;
 
 -- Estados --
@@ -61,26 +59,26 @@ create table grados (
 )
 
 set IDENTITY_INSERT grados ON;
-insert into grados values (1, 0.10);
+insert into grados (id_grado, muliplicador) values (1, 0.10);
 set IDENTITY_INSERT grados OFF;
 
 -- Publicaciones --
-create table publicaiones (
+create table publicaciones (
   id_publicacion int PRIMARY KEY NOT NULL IDENTITY(1, 1),
   id_estado int REFERENCES estados (id_estado),
-  id_grado int REFERENCES grados (id_grado),
+  id_grado tinyint REFERENCES grados (id_grado),
   stock smallint,
   direccion varchar(60),
   id_empresa int REFERENCES empresas (id_empresa),
-  id_rubro int REFERENCES rubros (id_rubro)
+  id_rubro tinyint REFERENCES rubros (id_rubro)
 );
 
-set IDENTITY_INSERT publicaiones ON;
+set IDENTITY_INSERT publicaciones ON;
 
 -- podríamos remplazar el join con estados
 -- dado que los que vienen estan todos en el estado 2
 
-insert into publicaiones (
+insert into publicaciones (
   id_publicacion,
   id_estado,
   id_grado,
@@ -90,19 +88,19 @@ insert into publicaiones (
 select distinct
   Espectaculo_Cod,
   E.id_estado,
-  1 -- grado de 0.1
+  1, -- grado de 0.1
   EM.id_empresa,
   1 -- rubro vacio
 from gd_esquema.Maestra M
 join estados E on M.Espectaculo_Estado = E.descripcion
 join empresas EM on EM.razon_social = M.Espec_Empresa_Razon_Social;
 
-SET IDENTITY_INSERT publicaiones OFF;
+SET IDENTITY_INSERT publicaciones OFF;
 
 -- Presentaciones --
 create table presentaciones (
   id_presentacion int PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  id_publicacion int REFERENCES publicaiones (id_publicacion),
+  id_publicacion int REFERENCES publicaciones (id_publicacion),
   fecha_hora datetime,
   fecha_venc datetime
 )
@@ -125,7 +123,7 @@ create table tipos_de_documentos (
 )
 
 SET IDENTITY_INSERT tipos_de_documentos ON;
-insert into tipos_de_documentos values (1, 'DNI'), (2, 'LC'), (3, 'LE');
+insert into tipos_de_documentos (id_tipo_de_documento, descripcion) values (1, 'DNI'), (2, 'LC'), (3, 'LE');
 SET IDENTITY_INSERT tipos_de_documentos OFF;
 
 -- Clientes --
@@ -210,13 +208,6 @@ from gd_esquema.Maestra M
 join medios_de_pago MP on M.Forma_Pago_Desc = MP.descripcion 
 where Factura_Nro is not null;
 
--- -- Items --
--- create table items (
---   id_item int PRIMARY KEY NOT NULL IDENTITY(1, 1),
---   id_factura int REFERENCES facturas (id_factura),
---   descripcion varchar(100),
--- )
-
 -- Tipos de ubicacion --
 create table tipos_de_ubicacion (
   id_tipo_de_ubicacion smallint PRIMARY KEY NOT NULL IDENTITY(1, 1),
@@ -239,14 +230,14 @@ SET IDENTITY_INSERT tipos_de_ubicacion OFF;
 -- Ubicaciones --
 create table ubicaciones (
   id_ubicacion int PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  id_publicacion int REFERENCES publicaiones (id_publicacion),
+  id_publicacion int REFERENCES publicaciones (id_publicacion),
   id_tipo_de_ubicacion smallint REFERENCES tipos_de_ubicacion (id_tipo_de_ubicacion),
-  fila tinyint,
+  fila char,
   asiento tinyint,
   precio int
 )
 
-insert into ubicacion (
+insert into ubicaciones (
   id_publicacion,
   id_tipo_de_ubicacion,
   fila,
@@ -263,24 +254,70 @@ from gd_esquema.Maestra M
 join tipos_de_ubicacion TU on M.Ubicacion_Tipo_Descripcion = TU.descripcion
 
 -- Compras --
-create table compras (
-  id_compra int PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  id_cliente REFERENCES clientes (id_cliente),
-  id_medio_de_pago REFERENCES medios_de_pago (id_medio_de_pago),
-  fecha datetime,
-  cantidad smallint,
-  id_presentacion int REFERENCES presentaciones (id_presentacion),
-  id_ubicacion int REFERENCES ubicaciones (id_ubicacion),
-  monto int
-);
+-- create table compras (
+--   id_compra int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   id_cliente int REFERENCES clientes (id_cliente),
+--   id_medio_de_pago REFERENCES medios_de_pago (id_medio_de_pago),
+--   fecha datetime,
+--   cantidad smallint,
+--   id_presentacion int REFERENCES presentaciones (id_presentacion),
+--   id_ubicacion int REFERENCES ubicaciones (id_ubicacion),
+--   monto int
+-- );
 
-insert into compras (
-  id_cliente,
-  id_medio_de_pago,
-  fecha
-)
-select C.id_cliente, MP.id_medio_de_pago, Compra_Fecha
-from gd_esquema.Maestra M
-join clientes C on C.numero_de_documento = M.Cli_Dni
-join medios_de_pago MP on MP.descripcion = M.Forma_Pago_Desc
-where Compra_Fecha is not null;
+-- insert into compras (
+--   id_cliente,
+--   id_medio_de_pago,
+--   fecha,
+--   cantidad,
+--   id_presentacion,
+--   id_ubicacion,
+--   monto
+-- )
+-- select C.id_cliente, MP.id_medio_de_pago, Compra_Fecha
+-- from gd_esquema.Maestra M
+-- join clientes C on C.numero_de_documento = M.Cli_Dni
+-- join medios_de_pago MP on MP.descripcion = M.Forma_Pago_Desc
+-- where Compra_Fecha is not null;
+
+-- select
+-- 	C.id_cliente,
+-- 	MP.id_medio_de_pago,
+-- 	Compra_Fecha,
+-- 	Compra_Cantidad,
+-- 	PRS.id_presentacion
+-- from gd_esquema.Maestra M
+-- join clientes C on C.numero_de_documento = M.Cli_Dni
+-- join medios_de_pago MP on MP.descripcion = M.Forma_Pago_Desc
+-- join presentaciones PRS on M.Espectaculo_Cod = PRS.id_espectaculo and M.Espectaculo_Fecha = PRS.fecha_hora and M.Espectaculo_Fecha_Venc = PRS.fecha_venc
+-- where Compra_Fecha is not null
+
+
+-- -- Items --
+-- create table items (
+--   id_item int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   id_factura int REFERENCES facturas (id_factura),
+--   descripcion varchar(100),
+--   id_compra int REFERENCES compras (id_compra),
+--   cantidad tinyint,
+--   comision decimal(6,2)
+-- )
+
+-- insert into items (
+--   id_factura,
+--   descripcion,
+--   id_compra,
+--   cantidad,
+--   comision
+-- )
+-- select distinct
+--   F.id_factura
+--   M.Item_Factura_Descripcion,
+--   C.id_compra,
+--   M.Item_Factura_Cantidad,
+--   M.Item_Factura_Monto
+-- from gd_esquema.Maestra M
+-- join facturas F on F.nro_factura = M.Factura_Nro
+-- join compras C on 
+--   C.id_presentacion = M.Espectaculo_Cod and
+--   C.fecha 
