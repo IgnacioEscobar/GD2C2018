0f561c75116fec17a1b4c441e8640ec5ca92624b
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-11-25 01:46:18 -0300

    Todos los datos a mostrar en ABM Rol listos

--- src/PalcoNet/Abm Rol/FormABMRol.cs
+++ src/PalcoNet/Abm Rol/FormABMRol.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using PalcoNet.funciones_utiles;
 
 namespace PalcoNet.Abm_Rol
 {
@@ -17,9 +20,59 @@ namespace PalcoNet.Abm_Rol
             InitializeComponent();
         }
 
+        private void mostrarRoles(SqlDataReader lector)
+        {
+            while (lector.Read())
+            {
+                lsbRoles.Items.Add(lector["descripcion"].ToString());
+            }
+        }
+
+        private void mostrarFuncionalidades(SqlDataReader lector)
+        {
+            while (lector.Read())
+            {
+                clbFuncionalidades.Items.Add(lector["descripcion"].ToString());
+            }
+        }
+
+        private void mostrarRolHabilitado(SqlDataReader lector)
+        {
+            if (lector.Read())
+            {
+                ckbActivo.Checked = Convert.ToBoolean(lector["habilitado"]);
+            }
+        }
+
         private void FormABMRol_Load(object sender, EventArgs e)
         {
+            GestorDB gestor = new GestorDB();
+            gestor.conectar();
+            gestor.consulta("SELECT descripcion FROM roles");
+            this.mostrarRoles(gestor.obtenerRegistros());
+            gestor.desconectar();
+        }
 
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            GestorDB gestor = new GestorDB();
+            gestor.conectar();
+            string query1 = "SELECT habilitado FROM roles WHERE descripcion = '" + lsbRoles.SelectedItem.ToString() + "'";
+            gestor.consulta(query1);
+            this.mostrarRolHabilitado(gestor.obtenerRegistros());
+            gestor.desconectar();
+            gestor.conectar();
+            string query2 = "SELECT R.habilitado, F.descripcion FROM dbo.roles R "
+                            + "JOIN dbo.funcionalidades_por_rol FR ON R.id_rol = FR.id_rol "
+                            + "JOIN dbo.funcionalidades F ON FR.id_funcionalidad = F.id_funcionalidad "
+                            + "WHERE R.descripcion = '" + lsbRoles.SelectedItem.ToString() + "'";
+            gestor.consulta(query2);
+            this.mostrarFuncionalidades(gestor.obtenerRegistros());
+            gestor.desconectar();
+        }
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
         }
 
     }
