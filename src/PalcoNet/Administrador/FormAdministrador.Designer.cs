c48315ecb5142c33ee92f9cce05259e2c6035932
santikhazki <santiagokhazki@gmail.com>
2018-11-25 11:36:17 -0300

    ventana adm modif

--- src/PalcoNet/Administrador/FormAdministrador.Designer.cs
+++ src/PalcoNet/Administrador/FormAdministrador.Designer.cs
@@ -29,31 +29,38 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.btnConfirmar);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(260, 238);
+            this.groupBox1.Size = new System.Drawing.Size(345, 208);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Administrador";
             // 
-            // listBox1
+            // btnConfirmar
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(6, 36);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 160);
-            this.listBox1.TabIndex = 0;
+            this.btnConfirmar.Location = new System.Drawing.Point(6, 56);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(95, 33);
+            this.btnConfirmar.TabIndex = 2;
+            this.btnConfirmar.Text = "ABM CLIENTE";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -64,20 +71,47 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "ABMs disponibles";
             // 
-            // btnConfirmar
+            // button1
             // 
-            this.btnConfirmar.Location = new System.Drawing.Point(150, 36);
-            this.btnConfirmar.Name = "btnConfirmar";
-            this.btnConfirmar.Size = new System.Drawing.Size(95, 33);
-            this.btnConfirmar.TabIndex = 2;
-            this.btnConfirmar.Text = "CONFIRMAR";
-            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(109, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ABM GRADO";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ABM RUBRO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(6, 155);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(95, 33);
+            this.button14.TabIndex = 16;
+            this.button14.Text = "ABM EMPRESA";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(208, 155);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(95, 33);
+            this.button15.TabIndex = 17;
+            this.button15.Text = "ABM ROL";
+            this.button15.UseVisualStyleBackColor = true;
             // 
             // FormAdministrador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(369, 228);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormAdministrador";
             this.Text = "PalcoNet";
@@ -92,6 +126,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnConfirmar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
