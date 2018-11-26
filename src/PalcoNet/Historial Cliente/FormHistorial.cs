904de868802b5e4b4a05170d0d9ca1c5f57a0250
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-11-26 10:27:59 -0300

    Un poco de Historial Cliente

--- src/PalcoNet/Historial Cliente/FormHistorial.cs
+++ src/PalcoNet/Historial Cliente/FormHistorial.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using PalcoNet.funciones_utiles;
 
 namespace PalcoNet.Historial_Cliente
 {
@@ -16,5 +19,43 @@ namespace PalcoNet.Historial_Cliente
         {
             InitializeComponent();
         }
+        /*
+                private void mostrarRegistros(SqlDataReader lector)
+                {
+                    while (lector.Read())
+                    {
+                        object[] row = new string[]
+                        {
+                            lector["nombre"].ToString(),
+                            lector["apellido"].ToString(),
+                            lector["numero_de_documento"].ToString()
+                        };
+                        dgvHistorial.Rows.Add(row);
+                    }
+                }
+
+                private void FormHistorial_Load(object sender, EventArgs e)
+                {
+                    dgvHistorial.ColumnCount = 3;
+                    dgvHistorial.ColumnHeadersVisible = true;
+                    dgvHistorial.Columns[0].Name = "NOMBRE";
+                    dgvHistorial.Columns[1].Name = "APELLIDO";
+                    dgvHistorial.Columns[2].Name = "DOCUMENTO";
+
+                    GestorDB gestor = new GestorDB();
+                    gestor.conectar();
+
+                    string query = "SELECT * " +
+                        "FROM PEAKY_BLINDERS.compras Co " +
+                        "JOIN PEAKY_BLINDERS.clientes Cl ON Co.id_cliente = Cl.id_cliente " +
+                        "WHERE Cl.id = ";
+ 
+                    ------- TODO: tener registrada la ID del cliente -------
+
+                    gestor.consulta("SELECT descripcion FROM PEAKY_BLINDERS.rubros");
+                    this.mostrarRegistros(gestor.obtenerRegistros());
+                    gestor.desconectar();
+                }
+         */
     }
 }
