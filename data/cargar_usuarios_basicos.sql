e94a83f2e79568c280a7727e57bef5d9f81f9bea
GabrielRuderman <gabriel_ruderman@hotmail.com>
2018-12-01 16:30:49 -0300

    Actualizacion de las cargas iniciales

--- data/cargar_usuarios_basicos.sql
+++ data/cargar_usuarios_basicos.sql
@@ -1,23 +1,20 @@
-INSERT INTO PEAKY_BLINDERS.usuarios (nombre_de_usuario, password_hash, habilitado, intentos_fallidos)
-VALUES ('admin', HASHBYTES ('SHA2_256', 'admin'), 1, 0)
+DELETE FROM PEAKY_BLINDERS.roles_por_usuario;
+DELETE FROM PEAKY_BLINDERS.usuarios;
 GO
 
-INSERT INTO PEAKY_BLINDERS.usuarios (nombre_de_usuario, password_hash, habilitado, intentos_fallidos)
-VALUES ('cliente', HASHBYTES ('SHA2_256', 'cliente'), 1, 0)
+DBCC CHECKIDENT ('PEAKY_BLINDERS.usuarios', RESEED, 0);
+DBCC CHECKIDENT ('PEAKY_BLINDERS.roles_por_usuario', RESEED, 0);
 GO
 
-INSERT INTO PEAKY_BLINDERS.usuarios (nombre_de_usuario, password_hash, habilitado, intentos_fallidos)
-VALUES ('empresa', HASHBYTES ('SHA2_256', 'empresa'), 1, 0)
+SET IDENTITY_INSERT PEAKY_BLINDERS.usuarios ON;
+INSERT INTO PEAKY_BLINDERS.usuarios (id_usuario, nombre_de_usuario, password_hash, habilitado, intentos_fallidos) VALUES
+	(1, 'admin', HASHBYTES ('SHA2_256', 'admin'), 1, 0),
+	(2, 'cliente', HASHBYTES ('SHA2_256', 'cliente'), 1, 0),
+	(3, 'empresa', HASHBYTES ('SHA2_256', 'empresa'), 1, 0)
+SET IDENTITY_INSERT PEAKY_BLINDERS.usuarios OFF;
 GO
 
-
-INSERT INTO PEAKY_BLINDERS.roles_por_usuario (id_usuario, id_rol)
-VALUES (1, 2)
-GO
-
-INSERT INTO PEAKY_BLINDERS.roles_por_usuario (id_usuario, id_rol)
-VALUES (2, 3)
-GO
-
-INSERT INTO PEAKY_BLINDERS.roles_por_usuario (id_usuario, id_rol)
-VALUES (3, 1)
+INSERT INTO PEAKY_BLINDERS.roles_por_usuario (id_usuario, id_rol) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3)
