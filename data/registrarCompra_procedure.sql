1d1ad6e9655cf8ab9720909e8c97468c86ab7c12
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-12-12 17:41:46 -0300

    monto desde tabla de ubicaciones en SP

--- data/registrarCompra_procedure.sql
+++ data/registrarCompra_procedure.sql
@@ -4,13 +4,15 @@ create procedure PEAKY_BLINDERS.registrarCompra
 @id_presentacion int,
 @id_publicacion int,
 @id_ubicacion int,
-@id_premio int,
-@monto int
+@id_premio int
 as
   begin
 	declare @monto_a_cobrar as int
 	declare @multiplicador_premio as decimal(3,2)
 	set @multiplicador_premio = 1
+
+  set @monto_a_cobrar = (select U.monto from PEAKY_BLINDERS.ubicaciones U where U.id_ubicacion = @id_ubicacion)
+
 	if @id_premio != -1
 		set @multiplicador_premio = (
       select TP.multiplicador from PEAKY_BLINDERS.premios P
@@ -18,7 +20,7 @@ as
 		  where P.id_cliente = @id_cliente and usado = 0 and P.id_premio = @id_premio
     )
   
-	set @monto_a_cobrar = @monto * @multiplicador_premio
+	set @monto_a_cobrar = @monto_a_cobrar * @multiplicador_premio
   -- registra compra con monto correspondiente, fecha actual y cantidad = 1
   insert into PEAKY_BLINDERS.compras (id_cliente, id_medio_de_pago, id_presentacion, id_publicacion, id_ubicacion, monto)
   values (@id_cliente, @id_medio_de_pago, @id_presentacion, @id_publicacion, @id_ubicacion, @monto_a_cobrar);
