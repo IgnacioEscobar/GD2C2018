23d528d3018b3eab0d118bf8941d066902fbebe9
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-12-20 01:00:07 -0300

    Inicializando configuracion

--- src/PalcoNet/Program.cs
+++ src/PalcoNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PalcoNet.funciones_utiles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace PalcoNet
         [STAThread]
         static void Main()
         {
+            Config.initialize();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormLogin());
